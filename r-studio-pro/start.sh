c547536bd787d07af5a0ea768ce0ecb4e6c43f19
Kerin Cosford <cosford@gmail.com>
2017-01-28 15:33:24 +0000

    Added RStudio Pro dockerfile

--- r-studio-pro/start.sh
+++ r-studio-pro/start.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 set -ex
 
-GROUP=rstudio
+# GROUP=rstudio
 
-while read USER; do
-  USER=$(echo $USER |
-    tr '[:upper:]' '[:lower:]' |
-    sed 's/.gov.uk$//g' |
-    sed 's/[^a-z0-9_]/-/g' |
-    cut -c1-32)
+# while read USER; do
+#   USER=$(echo $USER |
+#     tr '[:upper:]' '[:lower:]' |
+#     sed 's/.gov.uk$//g' |
+#     sed 's/[^a-z0-9_]/-/g' |
+#     cut -c1-32)
 
-  if ! getent passwd $USER > /dev/null 2>&1; then
-    sudo useradd -g $GROUP -m -d /mnt/$USER -s /dev/null $USER
-  fi
-done <$1
+#   if ! getent passwd $USER > /dev/null 2>&1; then
+#     sudo useradd -g $GROUP -m -d /mnt/$USER -s /dev/null $USER
+#   fi
+# done <$1
+
+# /usr/lib/rstudio-server/bin/rserver --server-daemonize 0
+
+USER=${USER:=rstudio}
+GROUP=staff
+
+if ! getent passwd $USER > /dev/null 2>&1; then
+    sudo useradd -g $GROUP -m -d /home/$USER -s /dev/null $USER
+    # sudo useradd -g $GROUP -m -d /home/$USER -s /bin/bash $USER
+    echo "${USER}:${USER}" | chpasswd
+fi
 
 /usr/lib/rstudio-server/bin/rserver --server-daemonize 0
