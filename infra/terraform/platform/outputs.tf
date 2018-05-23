c2c8581bf2885239c1c0ed5bdacabf494518595b
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2018-05-23 17:10:22 +0100

    Replace per-env terraform config with workspaces
    Terraform’s new(ish) workspace feature allows us to define terraform resource configuration once, and apply it to multiple conceptual environments, each with a different remote state behind them.
    
    This means that applying a change to two environments changes from:
    
    ```
    cd infra/terraform/environments/dev
    terraform apply
    cd ../alpha
    terraform apply
    ```
    
    to:
    
    ```
    cd infra/terraform/platform
    terraform workspace select dev
    terraform apply -var-file=dev.tfvars
    terraform workspace select alpha
    terraform apply -var-file=alpha.tfvars
    ```
    
    Crucially, the use of workspaces means that the same collection of terraform resources is used for both environments, so we no longer need to copy and paste changes between the two environments.
    
    **Important**: enabling workspaces changes the remote state store behind an environment, so simply running `terraform apply` on a workspace-enabled version will create duplicates of all resources, name clashes from the existing state, dogs and cats living together, and more. So as and when this PR is merged, we must manually move the existing state to the new workspace-enabled state store using `terraform state` commands.

--- infra/terraform/platform/outputs.tf
+++ infra/terraform/platform/outputs.tf
@@ -1,5 +1,5 @@
 output "env" {
-  value = "${var.env}"
+  value = "${terraform.workspace}"
 }
 
 output "control_panel_api_db_host" {
@@ -8,4 +8,12 @@ output "control_panel_api_db_host" {
 
 output "control_panel_api_iam_role_name" {
   value = "${module.control_panel_api.iam_role_name}"
+}
+
+output "airflow_db_host" {
+  value = "${module.airflow_db.address}"
+}
+
+output "airflow_efs_host" {
+  value = "${module.airflow_storage_efs_volume.dns_name}"
 }
